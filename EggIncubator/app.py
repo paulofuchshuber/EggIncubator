879cd273a4832898702b6857ec403410c4dedfd8
phfuks <paulo12372@gmail.com>
2021-09-20 15:58:39 -0300

    from prompt

--- EggIncubator/app.py
+++ EggIncubator/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect, session, g
 import boto3
 import time
+import datetime
 from boto3.dynamodb.conditions import Key, Attr
 
 app = Flask(__name__)
@@ -44,6 +45,10 @@ def login():
          
         pkID = request.form['email']
         password = request.form['password']
+
+        if len(pkID) == 0:     #se o campo de login estiver nulo...
+            print("#: Digite um Email!")
+            return redirect(url_for('login'))
 
         table = dynamodb.Table('EggIncubator')
         response = table.query(
@@ -123,10 +128,13 @@ def getGraphData():
     valuesAgain=[]
     for row in PairedList:
         #print (row[0],":",row[1])
-        labels.append(str(row[0]))
+    #   labels.append(str(row[0]))
+        labels.append(str(datetime.datetime.fromtimestamp(row[0])))
         values.append(row[1])
         valuesAgain.append(row[2])
-       
+    
+
+
     return (labels,values,valuesAgain)
 
 @app.route('/forms')
