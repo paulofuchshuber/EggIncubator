0ce08214c5de1f1d73543184dfead28b479048be
phfuks <paulo12372@gmail.com>
2021-10-28 16:30:06 -0300

    'sincronizando_git_rasp'

--- EggIncubator/PIDcontrol.py
+++ EggIncubator/PIDcontrol.py
@@ -37,20 +37,21 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 GPIO.setup(32,GPIO.OUT)
 
-p = GPIO.PWM(32,10)
+p = GPIO.PWM(32,5)
 p.start(0)
 
 tempRead=0
 setpoint = 38
-kP = 30
-kI = 1
-kD = 1
+kP = 50
+kI = 0.2
+kD = 0.2
 PID = 0
 P = 0
 I = 0
 D = 0
+potMax=90
 
-p.ChangeDutyCycle(70)
+p.ChangeDutyCycle(0)
 
 try:
     print("PWM Gerado!")
@@ -63,22 +64,18 @@ try:
         error = setpoint - tempRead
         P = error*kP
         I += error*kI
+        if (I >= potMax-50): #quando esta esquentando (inicialmente frio) nao acumular muito I
+            I=potMax-50
         D = (lastTemp-tempRead)*kD
-        PID = P + I + D
-        if (PID >= 50):
-            PID = 50
-            p.ChangeDutyCycle(70)
-            print("1 ",tempRead, error, PID)
-        elif (PID <= -50):
-            PID= -50
-            p.ChangeDutyCycle(0)
-            print("2 ",tempRead, error, PID)            
-        elif (PID < 50 and PID > 0):
-            p.ChangeDutyCycle(PID+50)
-            print("3 ",tempRead, error, PID)
-        elif (PID < 0):
-            p.ChangeDutyCycle(PID+50)
-            print("4 ",tempRead, error, PID)
+        PID = P + I + D 
+        print(round(PID,2))
+        if (PID >= potMax-50):  #limitar potencia max
+            PID = potMax-50
+        elif (PID <= -50):  #limitar potencia minima
+            PID = -50
+        p.ChangeDutyCycle(PID+50) #p.ChangeDutyCycle(PID+X) x=50 
+        #print("PID: ",round(PID,2),"I: ",round(I,2))
+        print("Temp: ",round(tempRead,2),"C  Erro: ", round(error,2),"C  Potencia: ",round(PID+50,2),"%") 
         
         
             
